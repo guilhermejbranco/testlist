b81fc0b397ae60a0164a94d9ce8f783c96c172c8
gujab <guilhasbranco@gmail.com>
2019-03-04 00:11:14 +0000

    Updates

---
+++ precache-manifest.8ee89c8223d641b6be6aa6aef7f172e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a11151695e45817a793b",
+    "url": "/guilhermejbranco/testlist.git/static/js/runtime~main.a1115169.js"
+  },
+  {
+    "revision": "fc7493d8d5a84eaff1a7",
+    "url": "/guilhermejbranco/testlist.git/static/js/main.fc7493d8.chunk.js"
+  },
+  {
+    "revision": "68348865077eb9a093ca",
+    "url": "/guilhermejbranco/testlist.git/static/js/2.68348865.chunk.js"
+  },
+  {
+    "revision": "fc7493d8d5a84eaff1a7",
+    "url": "/guilhermejbranco/testlist.git/static/css/main.c5312cfb.chunk.css"
+  },
+  {
+    "revision": "7d6f49714d6068ff32692f5054a6e790",
+    "url": "/guilhermejbranco/testlist.git/index.html"
+  }
+];
